e43f8cb087f0572fe8c2e3a385a8c12521ac8bf5
TiciaChance <laticia.chance@flatironschool.com>
2016-09-26 16:05:03 -0400

    getting started with the picker view func

--- MP-AutoLayoutWork/ViewController.swift
+++ MP-AutoLayoutWork/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    //data source for the picker and delegate to know when someone is swiping the view, when it stops and which data is selected so i can use it
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var chooseStateButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        statePicker.isHidden = true
+        
+        //when changes are made in statepicker look to the vc to see if they exist
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
     }
 
+    @IBAction func stateButtonTapped(_ sender: AnyObject) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return " "
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
